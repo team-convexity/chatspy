58a48af4c8a9912310c52558218a61e2053f3532
abdoulrasheed <abdulrasheedibrahim47@gmail.com>
2024-12-25 17:59:24 +0100

    convert escaped newlines (\n) into actual newlines before writing to file.

--- src/chatspy/secret.py
+++ src/chatspy/secret.py
@@ -1,4 +1,5 @@
 import os
+import codecs
 
 from logging import getLogger
 from .services import Service
@@ -9,17 +10,14 @@ logger = getLogger("gunicorn.info")
 class Secret:
     @classmethod
     def get_service_key(cls, service: Service, private: bool = False):
-        if settings.DEBUG:
-            if private:
-                cert = os.getenv(f"{service.value}_PRI_CERT")
-                if not cert:
-                    logger.warning(f"private cert for {service.name} is not found")
-                return cert
-
-            cert = os.environ.get(f"{service.value}_PUB_CERT")
+        if private:
+            cert = os.getenv(f"{service.value}_PRI_CERT")
             if not cert:
-                logger.warning(f"public cert for {service.name} is not found in the env")
-            return cert
-
-        # get cert from aws sec on staging, prod and qa.
-        return ...
+                logger.warning(f"private cert for {service.name} is not found")
+            return codecs.escape_decode(cert.encode("utf-8"))[0]
+        
+        cert = os.environ.get(f"{service.value}_PUB_CERT")
+        if not cert:
+            logger.warning(f"public cert for {service.name} is not found in the env")
+        
+        return codecs.escape_decode(cert.encode("utf-8"))[0]
